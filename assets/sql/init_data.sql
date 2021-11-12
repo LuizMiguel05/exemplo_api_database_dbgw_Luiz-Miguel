8a85de66b7b040ac610f5b0471b43e624f3d8917
LuizMiguel05 <josehenrique2362@gmail.com>
2021-11-12 11:30:47 +0000

    create_exemplobd

--- assets/sql/init_data.sql
+++ assets/sql/init_data.sql
@@ -1,1 +1,8 @@
 -- Código SQL com a inclusão dos registros iniciais do banco de dados
+
+INSERT INTO filmes(titulo) VALUES
+
+    ('Entre facas e segredos'),
+    ('O Hobbit'),
+    ('Guia do mochileiro das Glaláxias'),
+    ('Monty Python: Em Busca do Cálice Sagrado');
